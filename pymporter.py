02f9a538090564b47711677fc612c3d580ccbecf
Draghina Alexandru <alexandrei@gmx.net>
2009-06-13 19:42:12 +0300

    fix line endings add tags listings for a test file
    Signed-off-by: Draghina Alexandru <alexandrei@gmx.net>

--- pymporter.py
+++ pymporter.py
@@ -1,1 +1,20 @@
-#!/usr/bin/env python# -*- coding: utf-8 -*- '''Small script to copy photo files according to their EXIF data. ''' __version__ = '0.1'__author__ = u'Draghina Alexandru'.encode('utf-8')
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+'''
+Small script to copy photo files according to their EXIF data.
+ 
+'''
+ 
+__version__ = '0.1'
+__author__ = u'Draghina Alexandru'.encode('utf-8')
+
+import EXIF
+
+f = open("input/P6049984.JPG")
+#f = open("input/P6094556.ORF")
+tags = EXIF.process_file(f, details=False)
+f.close()
+for tag in tags.keys():
+  if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
+    print "%s: %s" % (tag, tags[tag])
