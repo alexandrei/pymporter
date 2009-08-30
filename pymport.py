d162dddba774682798b5261edba583e004ce3a19
Alexandru Draghina <alexandrei@gmx.net>
2009-08-30 23:54:15 +0300

    add file pymport.py, with just a basic skelleton

---
+++ pymport.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+""" Learning Python. 
+
+The final goal is to have a small script that imports and renames photos according to EXIF data
+"""
+
+import EXIF
+
+def main(args):
+	return -1
+
+if __name__ == '__main__':
+	sys.exit(main(sys.argv[1:]))
