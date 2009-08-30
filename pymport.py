f78528b7d8f8ca739609b0ab640bd162253683b3
Alexandru Draghina <alexandrei@gmx.net>
2009-08-31 00:26:10 +0300

    add code for listing files in folder

--- pymport.py
+++ pymport.py
@@ -6,30 +6,36 @@ The final goal is to have a small script that imports and renames photos accordi
 """
 
 import sys
+import os
 import EXIF
 
 __usage = """Specify the input folder!"""
 __version = """0.1"""
 
 def usage():
-	print __usage
-	
+    print __usage
+    
 def version():
-	print __version
+    print __version
+    
+def process_folder(_, dir_name, files):
+    for fname in files:
+        print fname
 
 def main(args):
-	if(not len(args)):
-		usage()
-		return -1
-	else:
-		return 0
+    if(not len(args)):
+        usage()
+        return -1
+        
+    os.path.walk('.', process_folder, None)
+    return 0
 
 if __name__ == '__main__':
-	if '--version' in sys.argv:
-		version()
-		sys.exit(0)
-	elif '--help' in sys.argv:
-		usage()
-		sys.exit(0)
-	else:
-	    sys.exit(main(sys.argv[1:]))
+    if '--version' in sys.argv:
+        version()
+        sys.exit(0)
+    elif '--help' in sys.argv:
+        usage()
+        sys.exit(0)
+    else:
+        sys.exit(main(sys.argv[1:]))
