35ef228f32f7b173b6a03c4c8c3f5aac20fa7a56
Alexandru Draghina <alexandrei@gmx.net>
2009-09-13 23:41:05 +0300

    use xml.etree
    Use xml.etree to internally store the files list, and changes, instead of arrays of objects

--- pymport.py
+++ pymport.py
@@ -8,11 +8,15 @@ The final goal is to have a small script that imports and renames photos accordi
 import sys
 import os
 import EXIF
+from xml.etree.ElementTree import ElementTree, Element
 
 __usage = """Specify the input folder!"""
 __version = """0.1"""
 __extensions = ('.jpg', '.jpeg')
 __raw_extensions = ('.orf')
+
+input_file_list = []
+raw_input_file_list = []
 
 def usage():
     print __usage
@@ -21,26 +25,33 @@ def version():
     print __version
 
 def main(args):
+    output = Element("files")
+    ElementTree(output).write(output_file)
     for root, dirs, files in os.walk(args[0]):
         for file_name in files:
             file_name_lower = file_name.lower()
             if file_name_lower.endswith(__extensions):
-                try:
-                    f = open(os.path.join(root,file_name), 'rb')
-                except IOError:
-                    print "Failed to open file %s" % os.path.join(root,file_name)
-                else:
-                    print "Opened file %s" % os.path.join(root,file_name)
-                    tags = EXIF.process_file(f, stop_tag='DateTimeOriginal', details = False)
-                    try:
-                        print tags['EXIF DateTimeOriginal']
-                    except KeyError:
-                        print "Failed to get 'EXIF DateTimeOriginal' for file %s" % os.path.join(root,file_name)
-                    f.close()
-            else:
-                print "Skip file %s" % os.path.join(root,file_name)
+                input_file_list.append(os.path.join(root,file_name))
+            if file_name_lower.endswith(__raw_extensions):
+                raw_input_file_list.append(os.path.join(root,file_name))
+                #try:
+                #    f = open(os.path.join(root,file_name), 'rb')
+                #except IOError:
+                #    print "Failed to open file %s" % os.path.join(root,file_name)
+                #else:
+                #    print "Opened file %s" % os.path.join(root,file_name)
+                #    tags = EXIF.process_file(f, stop_tag='DateTimeOriginal', details = False)
+                #    try:
+                #        print tags['EXIF DateTimeOriginal']
+                #    except KeyError:
+                #        print "Failed to get 'EXIF DateTimeOriginal' for file %s" % os.path.join(root,file_name)
+                #    f.close()
+            #else:
+            #    print "Skip file %s" % os.path.join(root,file_name)
         #if 'CVS' in dirs:
         #    dirs.remove('CVS')
+        
+        print input_file_list
     return 0
 
 if __name__ == '__main__':
