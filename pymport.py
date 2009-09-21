228ab417f1d7414b30fd57b080843491ef8ce7d8
Alexandru Draghina <alexandrei@gmx.net>
2009-09-22 00:30:40 +0300

    add code for splitting the date time in individual components

--- pymport.py
+++ pymport.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-""" Learning Python. 
+""" Learning Python.
 
 The final goal is to have a small script that imports and renames photos according to EXIF data
 """
@@ -17,30 +17,54 @@ __version = """0.3"""
 __extensions = ('.jpg', '.jpeg')
 __raw_extensions = ('.orf')
 __tags = ["Image DateTime", "EXIF DateTimeOriginal", "DateTime"]
+__base_gallery_path = "./output"
 
 def usage():
     print __usage
-    
+
 def version():
     print __version
-    
+
 def print_input_list(tree):
     dump(tree)
-    
+
 def build_output_names(list):
     files = list.getiterator("file")
-    
+
     for file in files:
         output = Element("output")
         file.append(output)
-        
+
         path = file.find("input").get("path")
         path = os.path.split(path)[0]
         ctime = file.find("exif").get("DateTime")
+
+        datestr = ctime.split(' ')
+        dd = datestr[0] #date
+        dt = datestr[1] #time
+
         ctime = ctime.replace(':', '')
         ctime = re.sub('[^\d]+', '_', ctime)
-        
-        output.set("path", path)
+
+        #year
+        y = dd.split(':')[0]
+
+        #month
+        if len(dd.split(':')[1]) < 2:
+          m = str('0') + dd.split(':')[1]
+        else:
+          m = dd.split(':')[1]
+
+        #day
+        if len(dd.split(':')[2]) < 2:
+          m = str('0') + dd.split(':')[2]
+        else:
+          m = dd.split(':')[2]
+
+        hr = dt.split(':')[0] #hour
+        mi = dt.split(':')[1] #minutes
+        se = dt.split(':')[1] #seconds
+
         output.set("datetime", ctime)
 
 def sort_jpeg_list(list):
@@ -48,17 +72,17 @@ def sort_jpeg_list(list):
     for elem in list:
         key = elem.find("input").get("path")
         data.append((key, elem))
-        
+
     data.sort()
 
     list[:] = [item[-1] for item in data]
-    
 
-    
+
+
 def match_raw_files(raw_list, jpeg_list):
     jpegs = jpeg_list.getiterator("file")
     raws = raw_list.getiterator("raw")
-    
+
     if (len(jpegs) > 0) and (len(raws) > 0):
         for jpeg in jpegs:
             for raw in raws:
@@ -66,17 +90,17 @@ def match_raw_files(raw_list, jpeg_list):
                 if jpeg_path is not None:
                     jpeg_path = os.path.splitext(jpeg_path)
                     jpeg_path = jpeg_path[0].lower()
-                    
+
                 raw_path = raw.get("path")
                 if raw_path is not None:
                     raw_path = os.path.splitext(raw_path)
                     raw_path = raw_path[0].lower()
-                
+
                 if jpeg_path == raw_path:
                     jpeg.append(copy.deepcopy(raw))
                     raw_list.remove(raw)
                     continue
-    
+
 def get_exif_data(list):
     files = list.getiterator("file")
     for file in files:
@@ -97,44 +121,44 @@ def get_exif_data(list):
                         continue
                 if dt_value:
                     file_exif.set("DateTime", dt_value)
-                    
+
             finally:
                 f.close()
-        
+
 
 def main(args):
     process = Element("process")
     jpeg_list = SubElement(process, "jpeg")
     raw_list = SubElement(process, "raw")
-    
+
     for root, dirs, files in os.walk(args[0]):
         for file_name in files:
             file_name_lower = file_name.lower()
             if file_name_lower.endswith(__extensions):
                 input_details = Element("input", {"path":os.path.join(root,file_name), "name":file_name})
-                
+
                 file_details = Element("file")
                 file_details.append(input_details)
-                
+
                 jpeg_list.append(file_details)
-                
+
             if file_name_lower.endswith(__raw_extensions):
                 file_details = Element("raw", {"path": os.path.join(root,file_name), "name":file_name})
-                
+
                 raw_list.append(file_details)
-                
+
     get_exif_data(jpeg_list)
-    
+
     match_raw_files(raw_list, jpeg_list)
-    
+
     sort_jpeg_list(jpeg_list)
-    
+
     build_output_names(jpeg_list)
-    
+
     print_input_list(process)
-    
+
     #ElementTree(output).write(output_file)
-    
+
     return 0
 
 if __name__ == '__main__':
