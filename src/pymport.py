d1a19d83ee747c9db20b9d1e39d26e0e0cdb31b2
Alexandru Draghina <alexandrei@gmx.net>
2009-10-10 02:45:46 +0300

    remove remaining functions, replaced by the new classes

--- src/pymport.py
+++ src/pymport.py
@@ -199,132 +199,32 @@ def add_output_date_time(list):
         output.set("minute", mi)
         output.set("second", se)
 
-
-def sort_jpeg_list(list):
-    data = []
-    for elem in list:
-        key = elem.find("input").get("path")
-        data.append((key, elem))
-
-    data.sort()
-
-    list[:] = [item[-1] for item in data]
-
-
-
-def match_raw_files(raw_list, jpeg_list):
-    jpegs = jpeg_list.getiterator("file")
-    raws = raw_list.getiterator("raw")
-
-    if (len(jpegs) > 0) and (len(raws) > 0):
-        for jpeg in jpegs:
-            for raw in raws:
-                jpeg_path = jpeg.find("input").get("path")
-                if jpeg_path is not None:
-                    jpeg_path = os.path.splitext(jpeg_path)
-                    jpeg_path = jpeg_path[0].lower()
-
-                raw_path = raw.get("path")
-                if raw_path is not None:
-                    raw_path = os.path.splitext(raw_path)
-                    raw_path = raw_path[0].lower()
-
-                if jpeg_path == raw_path:
-                    jpeg.append(copy.deepcopy(raw))
-                    raw_list.remove(raw)
-                    continue
-
-def get_exif_data(list):
-    files = list.getiterator("file")
-    for file in files:
-        input = file.find("input")
-        file_exif = SubElement(file, "exif")
-        try:
-            f = open(input.get("path"), 'rb')
-        except IOError:
-            print "Failed to open file %s" % input.get("path")
-        else:
-            try:
-                tags = EXIF.process_file(f, details = False)
-                for tag in __tags:
-                    try:
-                        dt_value = str(tags[tag])
-                        break
-                    except:
-                        continue
-                if dt_value:
-                    file_exif.set("DateTime", dt_value)
-
-            finally:
-                f.close()
-
-
 def main(args):
     
     #setup logging
-    logging.basicConfig(
-      filename=__logfile,
-      level = logging.DEBUG,
-      )
+    #===========================================================================
+    # logging.basicConfig(
+    #  filename=__logfile,
+    #  level = logging.DEBUG,
+    #  )
+    # 
+    # console = logging.StreamHandler()
+    # console.setLevel(logging.INFO)
+    # formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(messages)s')
+    # console.setFormatter(formatter)
+    # logging.getLogger('').addHandler(console)
+    # 
+    # logging.info('info message')
+    # logging.error('error message')
+    # logging.debug('debug message')
+    #===========================================================================
     
-    console = logging.StreamHandler()
-    console.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(messages)s')
-    console.setFormatter(formatter)
-    logging.getLogger('').addHandler(console)
-    
-    logging.info('info message')
-    logging.error('error message')
-    logging.debug('debug message')
-    
-    
-    
-    process = Element("process")
-    jpeg_list = SubElement(process, "jpeg")
-    raw_list = SubElement(process, "raw")
-
-    for root, dirs, files in os.walk(args[0]):
-        for file_name in files:
-            file_name_lower = file_name.lower()
-            if file_name_lower.endswith(__extensions):
-                input_details = Element("input", {"path":os.path.join(root,file_name), "name":file_name})
-
-                file_details = Element("file")
-                file_details.append(input_details)
-
-                jpeg_list.append(file_details)
-
-            if file_name_lower.endswith(__raw_extensions):
-                file_details = Element("raw", {"path": os.path.join(root,file_name), "name":file_name})
-
-                raw_list.append(file_details)
-
-    get_exif_data(jpeg_list)
-
-    match_raw_files(raw_list, jpeg_list)
-
-    sort_jpeg_list(jpeg_list)
-
-    add_output_date_time(jpeg_list)
-
-    create_groups(jpeg_list, __groups_time_delta)
-
-    print_input_list(process)
-
-    #ElementTree(output).write(output_file)
-
+    capp = ConverterApp("../input", "../output")
+    capp.get_files()
+    capp.read_meta()
+    capp.match_raw()
+ 
     return 0
 
 if __name__ == '__main__':
-    
-    if (len(sys.argv) < 2):
-        usage()
-        sys.exit(1)
-    elif '--version' in sys.argv:
-        version()
-        sys.exit(0)
-    elif '--help' in sys.argv:
-        usage()
-        sys.exit(0)
-    else:
-        sys.exit(main(sys.argv[1:]))
+    sys.exit(main(sys.argv[1:]))
